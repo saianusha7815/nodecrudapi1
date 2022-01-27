91d92ad58a349a4de78e49a64a33dacc3454e13f
Anusha Allaparthi <anusha.allaparthi@qentelli.com>
2022-01-27 22:56:44 +0530

    creating route

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const bodyParser = require('body-parser')
 const MongoClient=require('mongodb').MongoClient
 const app = express()
 app.use(bodyParser.urlencoded({extended:true}))
-const connectionString = ""
+
+//Db connection String
+const connectionString = "mongodb+srv://Saianusha:Saianusha@cluster0.apekh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+//connecting the db
 MongoClient.connect(connectionString,{useUnifiedTopology:true})
     .then(client => {
         console.log('connected to database-server')
         const db=client.db('star-wars-quotes')
         const quotesCollection=db.collection('quotes')
 
-        // 1.
+        // 1.create route with creating the quote
         app.post('/quotes',(req,res) => {
             quotesCollection.insertOne(req.body)
             .then(result => {
@@ -19,6 +23,14 @@ MongoClient.connect(connectionString,{useUnifiedTopology:true})
             .catch(error => console.error(error))
         })
 
+        // 2. Reading data from Mongodb
+        app.get('./getall',(req,res) => {
+            db.collection('quotes').find().toArray()
+            .then(result => {
+                res.send(result)
+            })
+            .catch(error => console.error(error))
+        })
 
 
     }).catch(console.error)
@@ -27,7 +39,7 @@ app.get('/',(req,res) => {
     res.sendFile(__dirname + './index.html')
 })
 
-const PORT=3000
+const PORT=4000
 app.listen(PORT,() => {
     console.log(`server running at port ${PORT}`)
 })
